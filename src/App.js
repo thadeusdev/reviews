8148f33e6ac4fb0d730d8d15d70724ff9dfc41ce
thadeus <thadeusnyariki94@gmail.com>
2023-03-05 22:04:46 +0300

    displayed review

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import Review from './Review'
-
+import React from 'react';
+import Review from './Review';
 function App() {
   return (
-    <main>
-      <section className="container">
-        <div className="title">
-          <h2>our reviews</h2>
-          <div className="underline"></div>
-        </div>
-      </section>
-    </main>
+  <main>
+    <section className="container">
+      <div className="title">
+        <h2>Our reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </section>
+  </main>
   );
 }
 
