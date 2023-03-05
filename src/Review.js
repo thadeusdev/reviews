49aa1d075b18ae74d742aeda7642361684dc9f6b
thadeus <thadeusnyariki94@gmail.com>
2023-03-05 10:01:27 +0300

    added review.js

---
+++ src/Review.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+    const [index, setIndex] = useState(0)
+    const {name, job, image, text} = people[index]
+  return (
+    <article className='review'>
+        <div className="img-container">
+            <img src={image} alt={name} className='person-img' />
+        </div>
+    </article>
+  );
+};
+
+export default Review;
